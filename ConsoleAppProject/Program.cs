869ba39358554ec34e4bdccdf8678d6fae8264d4
xybc4305158122 <lingxiumeng5@163.com>
2021-03-23 20:40:31 +0800

    Initial commit

---
+++ ConsoleAppProject/Program.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleAppProject.Helpers;
+using System;
+
+namespace ConsoleAppProject
+{
+    /// <summary>
+    /// The main method in this class is called first
+    /// when the application is started.  It will be used
+    /// to start Apps 01 to 05 for CO453 CW1
+    /// 
+    /// This Project has been modified by:
+    /// Derek Peacock 14/12/2020
+    /// </summary>
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            
+            Console.WriteLine("BNU CO453 Applications Programming 2020-2021!");
+            Console.WriteLine();
+
+            Console.WriteLine(MyEnum.FirstValue.EnumValue());
+            Console.WriteLine(MyEnum.FirstValue);
+           
+            
+            // Console.WriteLine(MyEnum.SecondValue.GetDescription());
+
+
+        }
+    }
+}
