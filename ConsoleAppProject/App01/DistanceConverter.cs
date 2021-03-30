394440093b768851d543756c1f7061b4201ecd65
xybc4305158122 <lingxiumeng5@163.com>
2021-03-31 05:36:11 +0800

    added some methods and attributes to distance converter class.

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -8,6 +8,31 @@
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
+        private double feet;
+
+        public void Run()
+        {
+
+        }
+    // Prompt the user to enter the miles 
+    // The miles will be in the double data type.
+        private void InputMiles()
+        {
+
+        }
+        // The method will calculate the feet
+        // The miles will be in the double data type.
+        private void CalculateFeet()
+        {
+
+        }
+        // Print out the output in feet.
+        // The miles will be in the double data type.
+        private void OutputFeet()
+        {
+
+        }
 
     }
 }
--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleAppProject
     /// to start Apps 01 to 05 for CO453 CW1
     /// 
     /// This Project has been modified by:
-    /// Derek Peacock 14/12/2020
+    /// Abdul Salam 14/12/2020
     /// </summary>
     public static class Program
     {
