5c57b5fc534a89c5d5cc7cc02fb430daedbfcaa8
xybc4305158122 <lingxiumeng5@163.com>
2024-12-30 00:25:16 +0800

    更新测试代码

--- test/matrix_test.py
+++ test/matrix_test.py
@@ -4,6 +4,9 @@ Gitee: https://gitee.com/walkline/micropython-ws2812-led-clock
 """
 from machine import Pin
 from neopixel import NeoPixel
+from config import Config
+from drivers.button import Button
+
 
 __neopixel = NeoPixel(Pin(14), 9 * 6)
 __neopixel.fill((128, 128, 128))
