baa004e0386c29556c59a985f6943bb572813e3e
xybc4305158122 <lingxiumeng5@163.com>
2025-02-24 23:20:16 +0800

    优化代码

--- main.py
+++ main.py
@@ -3,19 +3,21 @@ Copyright © 2021 Walkline Wang (https://walkline.wang)
 Gitee: https://gitee.com/walkline/micropython-ws2812-research
 """
 from machine import reset
-from config import Config
-from wifihandler import WifiHandler
-from matrix_clock import MatrixClock
+from utils.wifihandler import WifiHandler
 
+clock = None
 
-clock = MatrixClock()
 
 if __name__ == '__main__':
 	try:
-		if WifiHandler.STATION_CONNECTED == WifiHandler.set_sta_mode(Config.WIFI.SSID, Config.WIFI.PASSWORD):
+		if WifiHandler.STATION_CONNECTED == WifiHandler.set_sta_mode():
+			from matrix.matrix_clock import MatrixClock
+
+			clock = MatrixClock()
 			clock.mode = MatrixClock.MODE_TIME
 			clock.start()
 		else:
 			reset()
 	except KeyboardInterrupt:
-		clock.stop()
+		if clock:
+			clock.stop()
