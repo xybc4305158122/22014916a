557dad9cb6e965a971717c50c4beee432581c06c
xybc4305158122 <lingxiumeng5@163.com>
2025-04-04 06:38:46 +0800

    增加 Config 类

---
+++ config.py
@@ -0,0 +1,54 @@
+"""
+Copyright © 2021 Walkline Wang (https://walkline.wang)
+Gitee: https://gitee.com/walkline/micropython-ws2812-led-clock
+"""
+class Config(object):
+	# class WIFI(object):
+	# 	AP_SSID = 'Matrix Led Clock'
+	# 	AP_PASSWORD = ''
+	# 	AP_AUTHMODE = 0
+	# 	AP_HOST = "192.168.66.1"
+	# 	AP_PORT = 80
+	# 	AP_IFCONFIG = (AP_HOST, "255.255.255.0", AP_HOST, AP_HOST)
+	# 	AP_PORTAL = {'*': AP_HOST}
+
+
+	class PINS(object):
+		ADC = 1
+		DIN = 7
+
+
+	class KEYS(object):
+		KEY_1 = 2
+		KEY_2 = 3
+		KEY_3 = 4
+		KEY_4 = 5
+		KEY_TEST = 6
+		KEY_BOOT = 9
+
+		KEY_LIST = (KEY_1, KEY_2, KEY_3, KEY_4, KEY_TEST, KEY_BOOT)
+
+		KEY_MAP = {
+			KEY_1: 1,
+			KEY_2: 2,
+			KEY_3: 3,
+			KEY_4: 4,
+			KEY_TEST: 'TEST',
+			KEY_BOOT: 'BOOT'
+		}
+
+
+	class MATRIX(object):
+		HEIGHT = ROWS = 6
+		WIDTH = COLUMNS = 9
+		VERTICAL = True
+
+
+	class Colors(object):
+		BLACK = (0, 0, 0)
+		WHITE = (128, 128, 128)
+		RED = (255, 0, 0)
+		BLUE = (0, 0, 255)
+		GREEN = (0, 255, 0)
+		GREEN_MEDIUM = (128, 128, 0)
+		GREEN_LOW = (0, 60, 60)
