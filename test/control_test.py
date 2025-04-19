0876980abfdad42645db62eb4c74efa5c605bc53
xybc4305158122 <lingxiumeng5@163.com>
2025-04-19 17:08:46 +0800

    优化代码

--- test/control_test.py
+++ test/control_test.py
@@ -5,7 +5,19 @@ Gitee: https://gitee.com/walkline/micropython-ws2812-led-clock
 from drivers.button import Button
 from config import Config
 
-def buttons_click_cb(pin):
-	print(f'Key {Config.KEYS.KEY_MAP[pin]} clicked')
 
-buttons = Button(Config.KEYS.KEY_LIST, click_cb=buttons_click_cb)
+if __name__ == '__main__':
+	def buttons_click_cb(pin):
+		print(f'Key {Config.KEYS.KEY_MAP[pin]} clicked')
+
+	def buttons_press_cb(time, pin):
+		print(f'Key {Config.KEYS.KEY_MAP[pin]} pressed {time} ms')
+
+	buttons = Button(
+		Config.KEYS.KEY_LIST,
+		click_cb=buttons_click_cb,
+		press_cb=buttons_press_cb,
+		behavior=Button.BEHAVIOR_HOLD
+	)
+
+	print('try to click or hold keys on board')
