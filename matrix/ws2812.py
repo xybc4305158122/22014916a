4550f2531c8ecc3327c93317368c00e9dccb9efa
xybc4305158122 <lingxiumeng5@163.com>
2025-04-30 14:11:16 +0800

    优化逻辑，优化代码，增加函数注释

--- matrix/ws2812.py
+++ matrix/ws2812.py
@@ -11,19 +11,26 @@ class WS2812Matrix(object):
 	__BRIGHT_MAX = 0.1
 	__BRIGHT_MIN = 0.05
 
-	def __init__(self, width, height, pin=Config.PINS.HSPI_MOSI):
-		self.width = width
-		self.height = height
-		self.__neopixel = NeoPixel(Pin(pin), self.width * self.height)
+	def __init__(self, width, height, pin=Config.PINS.DIN):
+		self.__width = width
+		self.__height = height
+		self.__neopixel = NeoPixel(Pin(pin), self.__width * self.__height)
 		self.__bright_percent = 0
 		self.__bright_max = self.__BRIGHT_MAX
-		self.brightness = 100
+		self.__brightness = 100
+		self.powered_on = True
 
-	def clear(self):
+	def clean(self):
+		'''
+		清除屏幕（黑屏）
+		'''
 		self.__neopixel.fill(Config.Colors.BLACK)
 		self.show()
 
 	def fill(self, color:tuple(int, tuple)):
+		'''
+		填充指定颜色，参数支持 int 和 tuple (r, g, b)
+		'''
 		if isinstance(color, int):
 			color = int(color * self.__bright_percent)
 			self.__neopixel.fill((color, color, color))
@@ -31,15 +38,24 @@ class WS2812Matrix(object):
 			self.__neopixel.fill(self.set_color(color))
 
 	def show(self):
+		'''
+		显示所有指定的颜色
+		'''
 		self.__neopixel.write()
 
 	def refresh(self):
+		'''
+		刷新显示内容
+		'''
 		for index in range(self.led_count):
 			self.__neopixel[index] = self.set_color(self.__neopixel[index])
 
 		self.show()
 
 	def set_color(self, color:tuple):
+		'''
+		设置颜色亮度
+		'''
 		if isinstance(color, tuple) and len(color) == 3:
 			r, g, b = color
 			r = int(r * self.__bright_percent)
@@ -50,6 +66,9 @@ class WS2812Matrix(object):
 		return color
 
 	def set_bright_max(self, value=0.0):
+		'''
+		设置亮度最大值，取值范围 0.0~1.0
+		'''
 		if 1 < value <= 0:
 			self.__bright_max = self.__BRIGHT_MAX
 		else:
@@ -69,6 +88,9 @@ class WS2812Matrix(object):
 
 	@brightness.setter
 	def brightness(self, value:int):
+		if not self.powered_on:
+			return
+
 		if not isinstance(value, int) or value <= 10:
 			value = 100
 		elif value >= 100:
@@ -82,52 +104,42 @@ class WS2812Matrix(object):
 
 
 class WS2812MatrixClock(WS2812Matrix):
-	HOUR_TENS_PLACE_COLUMN = 0
-	HOUR_ONES_PLACE_COLUMN = 4
-	MINUTE_TENS_PLACE_COLUMN = 8
-	MINUTE_ONES_PLACE_LIST = [] # [5, 11, 17, 23, 29, 35, 41, 47, 53, 59]
+	__HOUR_TENS_PLACE_COLUMN = 0
+	__HOUR_ONES_PLACE_COLUMN = 4
+	__MINUTE_TENS_PLACE_COLUMN = 8
+	__MINUTE_ONES_PLACE_LIST = [] # [5, 11, 17, 23, 29, 35, 41, 47, 53, 59]
 
-	NUM_V0 = 0x7e3f # '111111000111111'
-	NUM_V1 = 0x27e1 # '010011111100001'
-	NUM_V2 = 0x5ebd # '101111010111101'
-	NUM_V3 = 0x56bf # '101011010111111'
-	NUM_V4 = 0x709f # '111000010011111'
-	NUM_V5 = 0x76b7 # '111011010110111'
-	NUM_V6 = 0x7eb7 # '111111010110111'
-	NUM_V7 = 0x421f # '100001000011111'
-	NUM_V8 = 0x7ebf # '111111010111111'
-	NUM_V9 = 0x76bf # '111011010111111'
+	__NUM_V0 = 0x7e3f # '111111000111111'
+	__NUM_V1 = 0x27e1 # '010011111100001'
+	__NUM_V2 = 0x5ebd # '101111010111101'
+	__NUM_V3 = 0x56bf # '101011010111111'
+	__NUM_V4 = 0x709f # '111000010011111'
+	__NUM_V5 = 0x76b7 # '111011010110111'
+	__NUM_V6 = 0x7eb7 # '111111010110111'
+	__NUM_V7 = 0x421f # '100001000011111'
+	__NUM_V8 = 0x7ebf # '111111010111111'
+	__NUM_V9 = 0x76bf # '111011010111111'
 
-	NUM_LIST_V = {
-		'0': NUM_V0,
-		'1': NUM_V1,
-		'2': NUM_V2,
-		'3': NUM_V3,
-		'4': NUM_V4,
-		'5': NUM_V5,
-		'6': NUM_V6,
-		'7': NUM_V7,
-		'8': NUM_V8,
-		'9': NUM_V9
+	__NUM_LIST_VERTICAL = {
+		'0': __NUM_V0,
+		'1': __NUM_V1,
+		'2': __NUM_V2,
+		'3': __NUM_V3,
+		'4': __NUM_V4,
+		'5': __NUM_V5,
+		'6': __NUM_V6,
+		'7': __NUM_V7,
+		'8': __NUM_V8,
+		'9': __NUM_V9
 	}
 
-	def __init__(self, width=10, height=6, vertical=True):
+	def __init__(self, width, height, vertical):
 		super().__init__(width, height)
 
-		self.MINUTE_ONES_PLACE_LIST = [self.height - 1 + self.height * count for count in range(width)]
+		self.__MINUTE_ONES_PLACE_LIST = [self.__height - 1 + self.__height * count for count in range(width)]
 		self.__vertical = vertical
-		self.__buf = bytearray(self.led_count * self.led_bpp)
 
 		self.set_brightness(self.brightness)
-
-	def power_on(self):
-		self.__neopixel.buf = self.__buf
-		self.show()
-	
-	def power_off(self):
-		self.__buf = bytearray(self.__neopixel.buf)
-		self.brightness = 0
-		self.show()
 
 	def set_brightness(self, value):
 		self.brightness = value
@@ -140,35 +152,35 @@ class WS2812MatrixClock(WS2812Matrix):
 
 	def set_hour(self, value:int):
 		hour = self.__zfill_time(value)
-		hour_tens = self.__zfile_bin(self.NUM_LIST_V[hour[0]])
-		hour_ones = self.__zfile_bin(self.NUM_LIST_V[hour[1]])
+		hour_tens = self.__zfile_bin(self.__NUM_LIST_VERTICAL[hour[0]])
+		hour_ones = self.__zfile_bin(self.__NUM_LIST_VERTICAL[hour[1]])
 
-		start = self.HOUR_TENS_PLACE_COLUMN * self.height
+		start = self.__HOUR_TENS_PLACE_COLUMN * self.__height
 		for count in range(3):
 			for index, bit in enumerate(hour_tens[count * 5 : count * 5 + 5]):
-				self.__neopixel[start + count * self.height + index] = self.__white if bit == '1' else self.__black
+				self.__neopixel[start + count * self.__height + index] = self.__white if bit == '1' else self.__black
 
-		start = self.HOUR_ONES_PLACE_COLUMN * self.height
+		start = self.__HOUR_ONES_PLACE_COLUMN * self.__height
 		for count in range(3):
 			for index, bit in enumerate(hour_ones[count * 5:count * 5 + 5]):
-				self.__neopixel[start + count * self.height + index] = self.__white if bit == '1' else self.__black
+				self.__neopixel[start + count * self.__height + index] = self.__white if bit == '1' else self.__black
 
 	def set_minute(self, value:int):
 		minute = self.__zfill_time(value)
 		minute_tens = int(minute[0])
 		minute_ones = int(minute[1])
 
-		start = self.MINUTE_TENS_PLACE_COLUMN * self.height
+		start = self.__MINUTE_TENS_PLACE_COLUMN * self.__height
 
 		if minute_tens == 0:
-			for index in range(start, start + self.height):
+			for index in range(start, start + self.__height):
 				self.__neopixel[index] = self.__black
 
 		for index in range(minute_tens):
 			self.__neopixel[start + index] = self.__blue
 
 		if minute_ones == 0:
-			for index in self.MINUTE_ONES_PLACE_LIST:
+			for index in self.__MINUTE_ONES_PLACE_LIST:
 				self.__neopixel[index] = self.__black
 
 		count = 0
@@ -181,7 +193,7 @@ class WS2812MatrixClock(WS2812Matrix):
 			elif 4 <= count <= 6:
 				green = self.__green_medium
 
-			self.__neopixel[self.MINUTE_ONES_PLACE_LIST[index]] = green
+			self.__neopixel[self.__MINUTE_ONES_PLACE_LIST[index]] = green
 
 	def __zfill_time(self, value:int):
 		'''将时分秒填充为 2 位数字符串'''
@@ -194,7 +206,7 @@ class WS2812MatrixClock(WS2812Matrix):
 
 
 if __name__ == '__main__':
-	test = WS2812MatrixClock()
+	test = WS2812MatrixClock(9, 6, True)
 
 	test.set_brightness(20)
 	test.set_hour(13)
