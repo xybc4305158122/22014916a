bd6e1eab8bf1d4c1ca8b3cdfb9b57d2238456f05
xybc4305158122 <lingxiumeng5@163.com>
2025-06-08 14:57:47 +0800

    增加光敏电阻测试内容

--- test/control_test.py
+++ test/control_test.py
@@ -2,13 +2,16 @@
 Copyright © 2021 Walkline Wang (https://walkline.wang)
 Gitee: https://gitee.com/walkline/micropython-ws2812-led-clock
 """
+from machine import Pin, ADC
 from drivers.button import Button
 from config import Config
+from utils.utilities import Utilities
 
 
 if __name__ == '__main__':
 	def buttons_click_cb(pin):
 		print(f'Key {Config.KEYS.KEY_MAP[pin]} clicked')
+		print(f'adc value: {adc.read()}')
 
 	def buttons_press_cb(time, pin):
 		print(f'Key {Config.KEYS.KEY_MAP[pin]} pressed {time} ms')
@@ -19,5 +22,9 @@ if __name__ == '__main__':
 		press_cb=buttons_press_cb,
 		behavior=Button.BEHAVIOR_HOLD
 	)
+
+	if Utilities.is_esp32c3():
+		adc = ADC(Pin(Config.PINS.ADC))
+		adc.atten(ADC.ATTN_11DB)
 
 	print('try to click or hold keys on board')
