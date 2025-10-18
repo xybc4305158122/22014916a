301d73fef46a5688dfff3750be749e4c84db4d36
xybc4305158122 <lingxiumeng5@163.com>
2025-10-19 00:13:17 +0800

    重构代码

--- main.py
+++ main.py
@@ -7,95 +7,60 @@ from utime import sleep
 from utils.utilities import Utilities
 from utils.wifihandler import WifiHandler
 from drivers.button import Button
-from drivers.photoresistor import Photoresistor
 from utils.dispatcher import Dispatcher
 
 clock = None
 buttons = None
-adc = None
 tasks = None
-last_adc_level = 0
-
-BRIGHTNESS_LEVEL = {
-	Photoresistor.LEVEL_1: 95,
-	Photoresistor.LEVEL_2: 75,
-	Photoresistor.LEVEL_3: 60,
-	Photoresistor.LEVEL_4: 45,
-	Photoresistor.LEVEL_5: 35,
-	Photoresistor.LEVEL_6: 25
-}
 
 
-def init_buttons():
-	global clock, buttons
+def buttons_click_cb(pin):
+	print(f'Key {Config.KEYS.KEY_MAP[pin]} clicked')
+	if not clock: return
 
-	def buttons_click_cb(pin):
-		print(f'Key {Config.KEYS.KEY_MAP[pin]} clicked')
-		if not clock: return
+	if pin == Config.KEYS.KEY_1:
+		clock.switch_mode()
+	elif pin == Config.KEYS.KEY_2:
+		pass
+	elif pin == Config.KEYS.KEY_3:
+		clock.set_brightness(clock.brightness - 20)
+		clock.show_time()
+	elif pin == Config.KEYS.KEY_4:
+		clock.switch_power()
 
-		if pin == Config.KEYS.KEY_1:
-			clock.switch_mode()
-		elif pin == Config.KEYS.KEY_2:
-			pass
-		elif pin == Config.KEYS.KEY_3:
-			clock.set_brightness(clock.brightness - 20)
-			clock.show_time()
-		elif pin == Config.KEYS.KEY_4:
-			clock.switch_power()
+def buttons_press_cb(time, pin):
+	print(f'Key {Config.KEYS.KEY_MAP[pin]} pressed {time} ms')
 
-	def buttons_press_cb(time, pin):
-		print(f'Key {Config.KEYS.KEY_MAP[pin]} pressed {time} ms')
-
-		if pin == Config.KEYS.KEY_1:
-			Utilities.delete_sta_config_file()
-			Utilities.hard_reset()
-
-	buttons = Button(
-		Config.KEYS.KEY_LIST,
-		click_cb=buttons_click_cb,
-		press_cb=buttons_press_cb
-	)
-
-# 定时器回调函数
-def auto_brightness():
-	global clock, adc, last_adc_level
-
-	adc_level = adc.level
-
-	if last_adc_level != adc_level:
-		last_adc_level = adc_level
-	else:
-		return
-
-	clock.set_brightness(BRIGHTNESS_LEVEL[adc_level])
-	clock.show_time()
-	print(f'set brightness level to {adc_level}')
+	if pin == Config.KEYS.KEY_1:
+		Utilities.delete_sta_config_file()
+		Utilities.hard_reset()
 
 
 if __name__ == '__main__':
 	try:
-		init_buttons()
+		buttons = Button(
+			Config.KEYS.KEY_LIST,
+			click_cb=buttons_click_cb,
+			press_cb=buttons_press_cb
+		)
 
 		if WifiHandler.STATION_CONNECTED == WifiHandler.set_sta_mode():
 			from matrix.matrix_clock import MatrixClock
 
-			clock = MatrixClock(Config.MATRIX.WIDTH, Config.MATRIX.HEIGHT, Config.MATRIX.VERTICAL)
+			clock = MatrixClock(Config.MATRIX.WIDTH, Config.MATRIX.HEIGHT)
 			clock.mode = MatrixClock.MODE_TIME
 			clock.start()
-
-			adc = Photoresistor(Config.PINS.ADC)
-			auto_brightness()
+			clock.auto_brightness()
 
 			tasks = Dispatcher()
-			tasks.add_work(auto_brightness, Config.PERIOD.ADC_MS)
 			tasks.add_work(clock.refresh_time, Config.PERIOD.CLOCK_MS)
+			tasks.add_work(clock.auto_brightness, Config.PERIOD.ADC_MS)
 
 			while True:
 				sleep(0.5)
 		else:
 			Utilities.hard_reset()
 	except KeyboardInterrupt:
+		if tasks: tasks.deinit()
 		if clock: clock.stop()
 		if buttons: buttons.deinit()
-		if adc: adc = None
-		if tasks: tasks.deinit()
--- matrix/matrix_clock.py
+++ matrix/matrix_clock.py
@@ -6,7 +6,8 @@ from config import Config
 from machine import RTC
 from utime import sleep, localtime
 import ntptime as ntp
-from .ws2812 import WS2812MatrixClock
+from .ws2812 import WS2812Matrix
+from drivers.photoresistor import Photoresistor
 from utils.wifihandler import WifiHandler
 
 TIMEZONE = 8
@@ -15,21 +16,63 @@ ntp.NTP_DELTA -= TIMEZONE * 60 * 60
 # ntp.host = 'ntp1.aliyun.com'
 
 
-class MatrixClock(WS2812MatrixClock):
+class MatrixClock(WS2812Matrix):
 	MODE_TIME = 0
 	MODE_LIGHT = 1
 	MODE_BLINK = 2
 	MODE_LIST = ['time', 'light', 'blink']
 
+	BRIGHTNESS_LEVEL = {
+		Photoresistor.LEVEL_1: 95,
+		Photoresistor.LEVEL_2: 75,
+		Photoresistor.LEVEL_3: 60,
+		Photoresistor.LEVEL_4: 45,
+		Photoresistor.LEVEL_5: 35,
+		Photoresistor.LEVEL_6: 25
+	}
+
 	__MODE_LAST = MODE_BLINK
 	__LIGHT_BRIGHT_MAX = 0.6
 
-	def __init__(self, width, height, vertical):
-		super().__init__(width=width, height=height, vertical=vertical)
+	__HOUR_TENS_PLACE_COLUMN = 0
+	__HOUR_ONES_PLACE_COLUMN = 4
+	__MINUTE_TENS_PLACE_COLUMN = 8
+	__MINUTE_ONES_PLACE_LIST = [] # [5, 11, 17, 23, 29, 35, 41, 47, 53, 59]
+
+	__NUM_V0 = 0x7e3f # '111111000111111'
+	__NUM_V1 = 0x27e1 # '010011111100001'
+	__NUM_V2 = 0x5ebd # '101111010111101'
+	__NUM_V3 = 0x56bf # '101011010111111'
+	__NUM_V4 = 0x709f # '111000010011111'
+	__NUM_V5 = 0x76b7 # '111011010110111'
+	__NUM_V6 = 0x7eb7 # '111111010110111'
+	__NUM_V7 = 0x421f # '100001000011111'
+	__NUM_V8 = 0x7ebf # '111111010111111'
+	__NUM_V9 = 0x76bf # '111011010111111'
+
+	__NUM_LIST_VERTICAL = {
+		'0': __NUM_V0,
+		'1': __NUM_V1,
+		'2': __NUM_V2,
+		'3': __NUM_V3,
+		'4': __NUM_V4,
+		'5': __NUM_V5,
+		'6': __NUM_V6,
+		'7': __NUM_V7,
+		'8': __NUM_V8,
+		'9': __NUM_V9
+	}
+
+	def __init__(self, width, height):
+		super().__init__(width, height)
+
+		self.__MINUTE_ONES_PLACE_LIST = [self.__height - 1 + self.__height * count for count in range(width)]
 
 		self.__rtc = RTC()
+		self.__adc = Photoresistor(Config.PINS.ADC)
 		self.__mode = self.MODE_TIME
 		self.__timer_count = 0
+		self.__last_adc_level = 0
 
 		self.clean()
 		self.set_brightness(20)
@@ -91,6 +134,7 @@ class MatrixClock(WS2812MatrixClock):
 
 	def stop(self):
 		self.__rtc = None
+		self.__adc = None
 
 	# 定时器回调函数
 	def refresh_time(self):
@@ -103,6 +147,82 @@ class MatrixClock(WS2812MatrixClock):
 			self.__timer_count = 0
 			self.sync_time()
 
+	# 定时器回调函数
+	def auto_brightness(self):
+		adc_level = self.__adc.level
+
+		if self.__last_adc_level != adc_level:
+			self.__last_adc_level = adc_level
+		else:
+			return
+
+		self.set_brightness(self.BRIGHTNESS_LEVEL[adc_level])
+		self.show_time()
+		print(f'set brightness level to {adc_level}')
+
+	def set_brightness(self, value):
+		self.brightness = value
+		self.__black = Config.Colors.BLACK
+		self.__white = self.set_color(Config.Colors.WHITE)
+		self.__blue = self.set_color(Config.Colors.BLUE)
+		self.__green = self.set_color(Config.Colors.GREEN)
+		self.__green_medium = self.set_color(Config.Colors.GREEN_MEDIUM)
+		self.__green_low = self.set_color(Config.Colors.GREEN_LOW)
+
+	def set_hour(self, value:int):
+		hour = self.__zfill_time(value)
+		hour_tens = self.__zfile_bin(self.__NUM_LIST_VERTICAL[hour[0]])
+		hour_ones = self.__zfile_bin(self.__NUM_LIST_VERTICAL[hour[1]])
+
+		start = self.__HOUR_TENS_PLACE_COLUMN * self.__height
+		for count in range(3):
+			for index, bit in enumerate(hour_tens[count * 5 : count * 5 + 5]):
+				self.__neopixel[start + count * self.__height + index] = self.__white if bit == '1' else self.__black
+
+		start = self.__HOUR_ONES_PLACE_COLUMN * self.__height
+		for count in range(3):
+			for index, bit in enumerate(hour_ones[count * 5:count * 5 + 5]):
+				self.__neopixel[start + count * self.__height + index] = self.__white if bit == '1' else self.__black
+
+	def set_minute(self, value:int):
+		minute = self.__zfill_time(value)
+		minute_tens = int(minute[0])
+		minute_ones = int(minute[1])
+
+		start = self.__MINUTE_TENS_PLACE_COLUMN * self.__height
+
+		# if minute_tens == 0:
+		for index in range(start, start + self.__height):
+			self.__neopixel[index] = self.__black
+
+		for index in range(minute_tens):
+			self.__neopixel[start + index] = self.__blue
+
+		# if minute_ones == 0:
+		for index in self.__MINUTE_ONES_PLACE_LIST:
+			self.__neopixel[index] = self.__black
+
+		count = 0
+		for index in range(minute_ones):
+			count += 1
+			green = self.__green
+
+			if 1 <= count <= 3:
+				green = self.__green_low
+			elif 4 <= count <= 6:
+				green = self.__green_medium
+
+			self.__neopixel[self.__MINUTE_ONES_PLACE_LIST[index]] = green
+
+	def __zfill_time(self, value:int):
+		'''将时分秒填充为 2 位数字符串'''
+		value = str(value)
+		return '0' + value if len(value) == 1 else value
+	
+	def __zfile_bin(self, value:int):
+		'''将整型转为 15 位二进制字符串'''
+		return '{:015b}'.format(value)
+
 	@property
 	def mode(self):
 		return self.__mode
@@ -113,3 +233,12 @@ class MatrixClock(WS2812MatrixClock):
 			value = self.MODE_TIME
 		
 		self.__mode = value
+
+
+if __name__ == '__main__':
+	test = MatrixClock(9, 6)
+
+	test.set_brightness(20)
+	test.set_hour(13)
+	test.set_minute(39)
+	test.show()
